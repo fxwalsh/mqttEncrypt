7fd6e68858eb2dde34ab753fe12bb0aeddd7f68b
Frank Walsh <fxwalsh@wit.ie>
2023-04-25 20:11:15 +0100

    first commit

---
+++ pub.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+import paho.mqtt.client as paho
+from paho import mqtt
+from urllib.parse import urlparse
+import sys
+import time
+import json
+from cryptography.fernet import Fernet
+
+# Define event callbacks
+def on_connect(client, userdata, flags, rc, properties=None):
+    print("Connection Result: " + str(rc))
+
+def on_publish(client, obj, mid, properties=None):
+    print("Message ID: " + str(mid))
+
+def encrypt_payload(payload):
+    #HAVING A HARD CODED KEY IS INSECURE  - SHOULD BE ENV/EXTERNAL VARIABLE
+    cypher_key=b'xqi9zRusHkcv3Om050HwX82eMTO-LbeW4YlqVVEzpw8=' 
+    cypher=Fernet(cypher_key)
+    encrypted_payload=cypher.encrypt(payload.encode('utf-8'))
+    return(encrypted_payload.decode())
+
+mqttc = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+
+# Assign event callbacks
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+
+url = urlparse("mqtt://806efec501e14bae9dde3d6b97243d9c.s1.eu.hivemq.cloud:8883/home")
+base_topic = url.path[1:]
+
+# Connect
+username="fxwalsh"
+password="Bzxy8n123"
+
+# enable TLS for secure connection
+mqttc.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+
+mqttc.username_pw_set(username, password)
+mqttc.connect(url.hostname, url.port)
+
+mqttc.loop_start()
+
+# Publish a message to temp every 15 seconds
+while True:
+    temp=30
+    temp_json=json.dumps({"temperature":temp, "timestamp":time.time()})
+    #mqttc.publish(base_topic+"/temperature", temp_json)
+    mqttc.publish(base_topic+"/temperature", encrypt_payload(temp_json))
+    time.sleep(15)
---
+++ sub.py
@@ -0,0 +1,51 @@
+import paho.mqtt.client as paho
+from paho import mqtt
+from urllib.parse import urlparse
+import sys
+import time
+import json
+from cryptography.fernet import Fernet
+
+# Define event callbacks
+def on_connect(client, userdata, flags, rc, properties=None):
+    print("Connection Result: " + str(rc))
+
+# print which topic was subscribed to
+def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+# print message, useful for checking if it was successful
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + decrypt_payload(msg.payload))
+
+def decrypt_payload(payload):
+    cypher_key=b'xqi9zRusHkcv3Om050HwX82eMTO-LbeW4YlqVVEzpw8=' #THIS IS VERY INSECURE - SHOULD BE ENV/EXTERNAL VARIABLE
+    cypher=Fernet(cypher_key)
+    decrypted_payload=cypher.decrypt(payload)
+    return(decrypted_payload.decode())
+
+mqttc = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+
+# Assign event cal  lbacks
+mqttc.on_connect = on_connect
+mqttc.on_subscribe = on_subscribe
+mqttc.on_message = on_message
+url = urlparse("mqtt://806efec501e14bae9dde3d6b97243d9c.s1.eu.hivemq.cloud:8883/home")
+base_topic = url.path[1:]
+
+# Connect
+username="fxwalsh"
+password="Bzxy8n123"
+
+# enable TLS for secure connection
+mqttc.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+
+mqttc.username_pw_set(username, password)
+mqttc.connect(url.hostname, url.port)
+mqttc.subscribe(base_topic+"/#")
+mqttc.loop_start()
+
+# Publish a message to temp every 15 seconds
+while True:
+    
+    time.sleep(15)
