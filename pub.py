3c587d1b01f678307c689f20908c05a625d28323
Frank Walsh <fxwalsh@wit.ie>
2023-04-25 20:15:09 +0100

    Update pub.py

--- pub.py
+++ pub.py
@@ -33,7 +33,7 @@ base_topic = url.path[1:]
 
 # Connect
 username="fxwalsh"
-password="Bzxy8n123"
+password="xxx"
 
 # enable TLS for secure connection
 mqttc.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
